649770d5646d50033bd6189ac552ac30ca8c94b4
Shakti Thandavan <srishakti193@gmail.com>
2022-11-01 22:45:12 +0100

    autoscaling

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -84,8 +84,6 @@ resource "aws_nat_gateway" "natgw" {
     Name = "gw NAT"
   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.gw]
 }
 resource "aws_default_route_table" "private_route_table" {
